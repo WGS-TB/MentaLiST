7b08c8a83c44fb3314f17b02d2b112db778fd4d1
Pedro Feijao <pedrofeijao1975@gmail.com>
2017-05-13 23:36:17 -0700

    reading new DB

--- src/mentalist.jl
+++ src/mentalist.jl
@@ -1,34 +1,14 @@
+include("build_db_functions.jl")
 using OpenGene
 using Logging
 using ArgParse
 using Bio.Seq
 using DataStructures
-import GZip
 
-function open_db{k}(::Type{DNAKmer{k}}, filename)
-
-  f = GZip.open("$filename")
-  k_from_file = parse(Int8, readline(f))
-  if k_from_file != k
-    Logging.critical("Wrong kmer size, DB has $k !")
-  end
-  kmer_db = DefaultDict{DNAKmer{k}, Vector{Tuple{String, Int8, Vector{Int16}}}}(() -> Vector{Tuple{String, Int8, Vector{Int16}}}())
-  loci = split(strip(readline(f)), ",")
-  for ln in eachline(f)
-    kmer, locus, val, alleles = split(ln, "\t")
-    push!(kmer_db[DNAKmer{k}(kmer)], (locus, parse(Int8, val), map(x->parse(Int16,x), split(alleles,","))))
-  end
-  close(f)
-  return kmer_db, loci
-end
 
 function parse_commandline()
     s = ArgParseSettings()
     @add_arg_table s begin
-        "-k"
-          help = "Kmer size"
-          required = true
-          arg_type = Int8
         "-o"
           help = "Output file with MLST call"
           arg_type = String
@@ -47,19 +27,20 @@ end
 
 function get_votes_for_sequence{k}(::Type{DNAKmer{k}}, seq, kmer_db, threshold=20)
   # stringMLST like pre_filter?
-  # half = div(length(seq),2)
-  # try
-  #   testmer = DNAKmer{k}(seq[half:half+k-1])
-  #   if !haskey(kmer_db, testmer)
-  #     return false, false
-  #   end
-  # catch
-  #   return false, false
-  # end
+  half = div(length(seq),2)
+  try
+    testmer = DNAKmer{k}(seq[half:half+k-1])
+    if !haskey(kmer_db, testmer)
+      return false, false
+    end
+  catch
+    return false, false
+  end
   # count all votes:
   votes = Dict()
   # count locus hits, for filtering
-  locus_hits = DefaultDict{String,Int16}(0)
+  # locus_hits = DefaultDict{String,Int16}(0)
+  locus_hits = DefaultDict{Int16,Int16}(0)
   for (pos, kmer) in each(DNAKmer{k}, DNASequence(seq), 1)
     kmer = canonical(kmer)
     if haskey(kmer_db, kmer)
@@ -89,11 +70,10 @@ end
 function main()
   Logging.configure(level=INFO)
   args = parse_commandline()
-  k::Int8 = args["k"]
   info("Opening kmer database ... ")
-  kmer_db, loci = open_db(DNAKmer{k}, args["db"])
-
-  votes = Dict(locus => DefaultDict{Int16, Int16}(0) for locus in loci)
+  kmer_db, loci, k = open_db(args["db"])
+  # votes = Dict(locus => DefaultDict{Int16, Int16}(0) for locus in loci)
+  votes = Dict(idx => DefaultDict{Int16, Int16}(0) for (idx,locus) in enumerate(loci))
   info("Opening fastq file(s) ... ")
   istream = fastq_open(args["f"])
   while (fq = fastq_read(istream))!=false
@@ -105,8 +85,9 @@ function main()
       end
     end
   end
-  println(join(loci,"\t"))
-  println(join([sort(collect(votes[locus]), by=x->-x[2])[1][1] for locus in loci],"\t"))
+  # println(join(loci,"\t"))
+  # println(join([sort(collect(votes[idx]), by=x->-x[2])[1][1] for (idx,locus) in enumerate(loci)],"\t"))
+
   # for locus in loci
     # sorted_vote = sort(collect(votes[locus]), by=x->-x[2])[1:2]
     # println("$locus:$sorted_vote")
