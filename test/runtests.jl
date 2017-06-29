683858c8821f0c20ade976a37a9a505440f8c66f
Dan Fornika <dfornika@gmail.com>
2017-06-28 19:04:09 -0700

    Added a few simple unit tests

---
+++ test/runtests.jl
@@ -0,0 +1,16 @@
+using Base.Test
+include("../src/build_db_functions.jl")
+
+@testset "complement_alleles" begin
+    @test complement_alleles([], 0) == Int16[]
+    @test complement_alleles([], 1) == [Int16(1)]
+    @test complement_alleles([-1], 0) == Int16[]
+    @test complement_alleles([-1], 1) == [Int16(1)]
+    @test complement_alleles([0], 0) == Int16[]
+    @test complement_alleles([0], 1) == [Int16(1)]
+    @test complement_alleles([1], 0) == Int16[]
+    @test complement_alleles([1], 1) == Int16[]
+    @test complement_alleles([], 0) == Int16[]
+end
+
+
