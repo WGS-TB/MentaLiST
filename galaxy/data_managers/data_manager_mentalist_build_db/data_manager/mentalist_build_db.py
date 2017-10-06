0e0238b8a5a338e549c82b36a4699f49bc4ebd77
Dan Fornika <dfornika@gmail.com>
2017-10-06 15:53:53 -0700

    Update galaxy data managers and tools

--- galaxy/data_managers/data_manager_mentalist_build_db/data_manager/mentalist_build_db.py
+++ galaxy/data_managers/data_manager_mentalist_build_db/data_manager/mentalist_build_db.py
@@ -11,14 +11,18 @@ from json import dumps, loads
 DEFAULT_DATA_TABLE_NAMES = ["mentalist_databases"]
 
 
-def build_mentalist_database( data_manager_dict, database_name, kmer_size, profile, fasta_files, params, target_directory, dbkey, data_table_names=DEFAULT_DATA_TABLE_NAMES ):
-    args = [ 'mentalist', 'build_db', '--db', database_name, '-k', kmer_size, '--profile', profile, '--fasta_files', fasta_files]
+def build_mentalist_database( data_manager_dict, database_name, kmer_size, profile, fasta_files, params, target_directory, data_table_names=DEFAULT_DATA_TABLE_NAMES ):
+    args = [ 'mentalist', 'build_db', '--db', database_name, '-k', str(kmer_size)]
+    if profile:
+        args += ['--profile', profile]
+    print(args)
+    args += ['--fasta_files'] + fasta_files
     proc = subprocess.Popen( args=args, shell=False, cwd=target_directory )
     return_code = proc.wait()
     if return_code:
         print("Error building database.", file=sys.stderr)
         sys.exit( return_code )
-    data_table_entry = dict( value=database_name, dbkey=dbkey, name=database_name, path=database_name )
+    data_table_entry = dict( value=database_name, dbkey=database_name, name=database_name, path=(database_name + '/' + database_name) )
     for data_table_name in data_table_names:
         _add_data_table_entry( data_manager_dict, data_table_name, data_table_entry )
 
@@ -37,7 +41,6 @@ def main():
     parser.add_argument( '-f', '--fasta_files', dest='fasta_files', nargs='+', default=None, help='fasta_filenames' )
     parser.add_argument( '-k', '--kmer_size', dest='kmer_size', type=int, default=None, help='kmer_size' )
     parser.add_argument( '-p', '--profile', dest='profile', type=int, default=None, help='kmer_size' )
-    parser.add_argument( '-y', '--dbkey', dest='dbkey', default=None, help='dbkey' )
     args = parser.parse_args()
 
     params = loads( open( args.params ).read() )
@@ -53,13 +56,8 @@ def main():
 
     data_manager_dict = {}
 
-    dbkey = args.dbkey
-
-    if dbkey in [ None, '', '?' ]:
-        raise Exception( '"%s" is not a valid dbkey. You must specify a valid dbkey.' % ( dbkey ) )
-
     # build the index
-    build_mentalist_database( data_manager_dict, args.database_name, args.kmer_size, args.profile, args.fasta_files, params, target_directory, dbkey, DEFAULT_DATA_TABLE_NAMES )
+    build_mentalist_database( data_manager_dict, args.database_name, args.kmer_size, args.profile, args.fasta_files, params, target_directory, DEFAULT_DATA_TABLE_NAMES )
 
     # save info to json file
     open( args.params, 'wb' ).write( dumps( data_manager_dict ) )
