46241499a1dbb4b92b3d49f9fc98caab85705013
Dan Fornika <dfornika@gmail.com>
2017-10-10 16:27:53 -0700

    Update mentalist.rb

--- homebrew/mentalist.rb
+++ homebrew/mentalist.rb
@@ -3,7 +3,9 @@ class Mentalist < Formula
   homepage "https://github.com/WGS-TB/MentaLiST"
   url "https://github.com/WGS-TB/MentaLiST/archive/v0.1.2.tar.gz"
   sha256 "0121110f87264423d9ffd18a6262753b1d643a84cbaabf0842c0d2f909d7ccf6"
-
+  
+  depends_on "julia"
+  
   def install
 
   end
