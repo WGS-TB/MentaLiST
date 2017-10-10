420d6aac361a7a6372d8e55e58fd0bd61d2ffc4a
Dan Fornika <dfornika@gmail.com>
2017-10-10 16:23:14 -0700

    Update mentalist.rb

--- homebrew/mentalist.rb
+++ homebrew/mentalist.rb
@@ -4,8 +4,6 @@ class Mentalist < Formula
   url "https://github.com/WGS-TB/MentaLiST/archive/v0.1.2.tar.gz"
   sha256 "0121110f87264423d9ffd18a6262753b1d643a84cbaabf0842c0d2f909d7ccf6"
 
-  # depends_on "cmake" => :build
-
   def install
 
   end
