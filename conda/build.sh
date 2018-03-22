3c3d4298ccef728ce8af5195271901e84aadd6aa
Dan Fornika <dfornika@gmail.com>
2018-03-22 11:41:49 -0700

    Update conda recipe

--- conda/build.sh
+++ conda/build.sh
@@ -6,12 +6,14 @@ ln -s $PREFIX/bin/MentaLiST.jl $PREFIX/bin/mentalist
 chmod +x $PREFIX/bin/mentalist
 
 julia -e 'Pkg.init()'
-julia -e 'Pkg.add("Suppressor")'
 julia -e 'Pkg.add("ArgParse")'
 julia -e 'Pkg.add("Bio")'
 julia -e 'Pkg.add("OpenGene")'
 julia -e 'Pkg.add("Logging")'
 julia -e 'Pkg.add("Lumberjack")'
+julia -e 'Pkg.add("FastaIO")'
+julia -e 'Pkg.add("JLD")'
+julia -e 'Pkg.add("DataStructures")'
 
 rm -f "$PREFIX"/share/julia/site/lib/v*/*.ji
 rm -rf "$PREFIX"/share/julia/site/v*/METADATA
