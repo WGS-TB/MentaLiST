78fcb174b47d18771b999f5050174b7fb20f8d77
Dan Fornika <dfornika@gmail.com>
2018-03-29 11:49:25 -0700

    Add Data Manager Mentalist Remove DB

--- galaxy/data_managers/data_manager_mentalist_remove_db/data_manager/mentalist_remove_db.py
+++ galaxy/data_managers/data_manager_mentalist_remove_db/data_manager/mentalist_remove_db.py
@@ -2,50 +2,29 @@
 from __future__ import print_function
 
 import argparse
+import json
 import os
-import subprocess
-import sys
-import errno
-from json import dumps, loads
-
-DEFAULT_DATA_TABLE_NAMES = ["mentalist_databases"]
+import shutil
 
 
-def mentalist_remove_db( data_manager_dict, database_name, params, data_table_names=DEFAULT_DATA_TABLE_NAMES ):
-    data_table_entry = dict( value=database_name, dbkey=database_name, name=database_name, path=database_name )
-    for data_table_name in data_table_names:
-        _remove_data_table_entry( data_manager_dict, data_table_name, data_table_entry )
+def main(args):
+    data_manager_entry = {}
+    data_manager_entry['value'] = args.database.lower()
+    data_manager_entry['name'] = args.database
+    data_manager_entry['path'] = '.'
+    data_manager_json = dict(data_tables=dict(kraken_databases=data_manager_entry))
+    params = json.loads(open(args.output).read())
+    target_directory = params['output_data'][0]['extra_files_path']
+    os.mkdir(target_directory)
+    output_path = os.path.join(os.getcwd(), 'kraken-database')
+    for filename in os.listdir(output_path):
+        shutil.move(os.path.join(output_path, filename), target_directory)
+    file(args.output, 'w').write(json.dumps(data_manager_json))
 
 
-def _remove_data_table_entry( data_manager_dict, data_table_name, data_table_entry ):
-    data_manager_dict['data_tables'] = data_manager_dict.get( 'data_tables', {} )
-    data_manager_dict['data_tables'][ data_table_name ] = data_manager_dict['data_tables'].get( data_table_name, [] )
-    data_manager_dict['data_tables'][ data_table_name ].append( data_table_entry )
-    return data_manager_dict
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('params')
-    parser.add_argument( '-d', '--db', dest='database_name', default=None, help='Database Name' )
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create data manager json.')
+    parser.add_argument('--db', dest='database', action='store', help='Database name')
+    parser.add_argument('--out', dest='output', action='store', help='JSON filename')
     args = parser.parse_args()
-
-    params = loads( open( args.params ).read() )
-    target_directory = params[ 'output_data' ][0]['extra_files_path']
-
-    try:
-        os.mkdir( target_directory )
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir( target_directory ):
-            pass
-        else:
-            raise
-
-    data_manager_dict = {}
-
-    # save info to json file
-    open( args.params, 'wb' ).write( dumps( data_manager_dict ) )
-
-
-if __name__ == "__main__":
-    main()
+    main(args)
