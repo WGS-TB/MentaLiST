8159aa6d954787332896df98ebe9d3b1feb4ffb6
Reza Miraskarshahi <rezamiraskarshahi@Rezas-MacBook-Air.local>
2019-10-14 00:55:24 -0700

    New ILP

--- src/kmer_coverage.jl
+++ src/kmer_coverage.jl
@@ -5,10 +5,12 @@ using Distributed
   # one minute at most per locus, gap=0.1, no need to have optimal per se;
   # One thread per solve, since every ILP is already being run in parallel.
   m = Model(solver=GurobiSolver(OutputFlag=0, MIPGap=0.1, TimeLimit=1200, Threads=1))
+  # m = Model(with_optimizer(Gurobi.Optimizer, OutputFlag=0, MIPGap=0.1, TimeLimit=1200, Threads=1))
   n_kmer = length(kmer_class)
   n_alleles = length(allele_ids)
   # kmer decision variable:
   @variable(m, x[1:n_kmer], Bin)
+  @variable(m, z[1:n_kmer], Int) #NOTE: NEW
   @variable(m, c[1:n_alleles], Int)
   # coverage constraints:
   kmer_list = collect(keys(kmer_class))
@@ -27,6 +29,9 @@ using Distributed
     @constraint(m, c[j] >= coverages[j])
     # @constraint(m, c[j] == coverages[j])
   end
+  for (i, kmer) in enumerate(kmer_list)
+    @constraint(m, z[i] == length(kmer_class[kmer])) #NOTE: NEW
+  end
   # Forcing same coverage: (takes much longer, get many more kmers; better to store coverage)
   # Update: consider the cardinality of the kmers, to minimize it also:
   # @variable(m, max_card, Int) # max cardinality of the selected kmers
@@ -34,7 +39,9 @@ using Distributed
   #   @constraint(m, max_card >= x[i] * length(kmer_class[kmer]))
   # end
   # minimize # of selected kmers:
-  @objective(m, Min, sum(x[i] for i in 1:n_kmer))
+
+  @objective(m, Min, sum(x[i]*z[i] for i in 1:n_kmer)) #NOTE: NEW
+  # @objective(m, Min, sum(x[i] for i in 1:n_kmer))
   # @objective(m, Min, sum(x[i] for i in 1:n_kmer) + 100000*max_card)
   # TODO: e parameters works well, reduces variance but increases kmer number as expected.
   status = solve(m)
